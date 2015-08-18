e572886b82336681f257dfe717c5b19df06ab086
Garth MacKenzie <garthmac@juno.com>
2015-08-17 22:18:48 -0600

    Rev 0

--- Trax/ViewController.swift
+++ Trax/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //sign up to hear about GPX files arriving
+        
+        let center = NSNotificationCenter.defaultCenter()
+        let queue = NSOperationQueue.mainQueue()
+        let appDelegate = UIApplication.sharedApplication().delegate
+        
+        center.addObserverForName(GPXURL.Notification, object: appDelegate, queue: queue) { notification in
+            if let url = notification?.userInfo?[GPXURL.Key] as? NSURL {
+                self.textView.text = "Received URL = \(url)"
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
